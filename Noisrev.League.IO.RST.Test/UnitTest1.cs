d93e430bedaaef55564e59c4087c0a2b67f4fcd1
Noisr <Noisr@DESKTOP-4KH5L54>
2021-07-01 22:05:09 +0800

    Add project files.

---
+++ Noisrev.League.IO.RST.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Noisrev.League.IO.RST.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Noisrev.League.IO.RST/RSTFile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noisrev.League.IO.RST
+{
+    class RSTFile
+    {
+    }
+}
