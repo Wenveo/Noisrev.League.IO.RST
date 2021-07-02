653a1089bfd402846487adbc70a519c17d0e2554
Noisrev <Noisrev@outlook.com>
2021-07-02 17:05:39 +0800

    test commit

---
+++ Noisrev.League.IO.RST/RSTHash.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noisrev.League.IO.RST
+{
+    public static class RSTHash
+    {
+        public static ulong Comput()
+        {
+            return 0;
+        }
+    }
+}
