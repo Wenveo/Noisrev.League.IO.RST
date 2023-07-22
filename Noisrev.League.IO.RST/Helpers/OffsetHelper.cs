7e0c39338292bcda53c59591b3495e427ec880c6
Wenveo <Wenveo@outlook.com>
2023-07-22 14:44:06 +0800

    Fix bad commit, Reset to the  9ce250993d89b606a32db576d6cfa6625c56a6cd submit

--- Noisrev.League.IO.RST/Helpers/OffsetHelper.cs
+++ Noisrev.League.IO.RST/Helpers/OffsetHelper.cs
@@ -22,6 +22,6 @@ public static class OffsetHelper
     /// <exception cref="OverflowException"/>
     public static ulong ComputeOffset(this long offset, RType type)
     {
-        return Convert.ToUInt64(offset << (byte)type);
+        return (ulong)offset << (byte)type;
     }
 }
