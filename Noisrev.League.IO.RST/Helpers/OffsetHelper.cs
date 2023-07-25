39c37cf92ea12e2b978b81842206a9f0b291a177
Wenveo <Wenveo@outlook.com>
2023-07-25 13:44:08 +0800

    Fix overflow exception caused by incorrect type conversion (#8)

--- Noisrev.League.IO.RST/Helpers/OffsetHelper.cs
+++ Noisrev.League.IO.RST/Helpers/OffsetHelper.cs
@@ -14,14 +14,14 @@ namespace Noisrev.League.IO.RST.Helpers;
 public static class OffsetHelper
 {
     /// <summary>
-    /// Use <see cref="RType"/> to generate offsets for RST elements.
+    /// Use <see cref="RType"/> to generate offsets for that text of <see cref="RSTFile"/>.
     /// </summary>
-    /// <param name="offset">The offset</param>
-    /// <param name="type">The type</param>
+    /// <param name="offset">The offset of that text.</param>
+    /// <param name="type">The type of that <see cref="RSTFile"/>.</param>
     /// <returns>Returns the generated offset.</returns>
     /// <exception cref="OverflowException"/>
     public static ulong ComputeOffset(this long offset, RType type)
     {
-        return Convert.ToUInt64(offset << (byte)type);
+        return (ulong)offset << (byte)type;
     }
 }
